ccb10bade568a3513c3e43a467f0895f19b1f969
dev-aswani <dev.aswani1505@gmail.com>
2024-07-26 15:26:19 +0400

    Availability Calendar Error

--- src/actions/AddProfilePhoto.js
+++ src/actions/AddProfilePhoto.js
@@ -1,25 +1,27 @@
-'use server'
+"use server";
 
-import { auth } from '@/auth';
+import { auth } from "@/auth";
 import { User } from "../../models/userModel";
 import { Store } from "../../models/storeModel";
 
 import { connectDB } from "../utils/connect";
 
 export async function AddProfilePhoto(formData) {
-    const session = await auth();
-    const email = session.user?.email;
-    const profileImg = formData.get("profileImg");
+  const session = await auth();
+  const email = session.user?.email;
+  const profileImg = formData.get("profileImg");
 
-    console.log(profileImg)
+  //     console.log(profileImg)
 
-     // CONNECT DB
-     await connectDB();
- 
-     await Store.updateOne({ownerEmail: email},{
-        $set : {        
-                img: profileImg
-        }
-     });
+  // CONNECT DB
+  await connectDB();
 
+  await Store.updateOne(
+    { ownerEmail: email },
+    {
+      $set: {
+        img: profileImg,
+      },
+    }
+  );
 }
